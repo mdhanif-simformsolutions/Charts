d651b7115336e8f6d31e6579cc0aeddcff5b57a8
Md Hanif <mohammed.h@simformsolutions.com>
2022-03-25 17:04:09 +0530

    - Updated Click event issue

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -39,10 +39,10 @@ open class ChartDataSet: ChartBaseDataSet
     
     @objc public init(entries: [ChartDataEntry], label: String)
     {
-        self.entries = entries 
-
+        self.entries = entries
+        
         super.init(label: label)
-
+        
         self.calcMinMax()
     }
     
@@ -52,13 +52,13 @@ open class ChartDataSet: ChartBaseDataSet
     }
     
     // MARK: - Data functions and accessors
-
+    
     /// - Note: Calls `notifyDataSetChanged()` after setting a new value.
     /// - Returns: The array of y-values that this DataSet represents.
     /// the entries that this dataset represents / holds together
     @objc
     open private(set) var entries: [ChartDataEntry]
-
+    
     /// Used to replace all entries of a data set while retaining styling properties.
     /// This is a separate method from a setter on `entries` to encourage usage
     /// of `Collection` conformances.
@@ -69,7 +69,7 @@ open class ChartDataSet: ChartBaseDataSet
         self.entries = entries
         notifyDataSetChanged()
     }
-
+    
     /// maximum y-value in the value array
     internal var _yMax: Double = -Double.greatestFiniteMagnitude
     
@@ -88,9 +88,9 @@ open class ChartDataSet: ChartBaseDataSet
         _yMin = Double.greatestFiniteMagnitude
         _xMax = -Double.greatestFiniteMagnitude
         _xMin = Double.greatestFiniteMagnitude
-
+        
         guard !isEmpty else { return }
-
+        
         forEach(calcMinMax)
     }
     
@@ -98,7 +98,7 @@ open class ChartDataSet: ChartBaseDataSet
     {
         _yMax = -Double.greatestFiniteMagnitude
         _yMin = Double.greatestFiniteMagnitude
-
+        
         guard !isEmpty else { return }
         
         let indexFrom = entryIndex(x: fromX, closestToY: .nan, rounding: .down)
@@ -197,9 +197,11 @@ open class ChartDataSet: ChartBaseDataSet
     open override func entriesForXValue(_ xValue: Double) -> [ChartDataEntry]
     {
         let match: (ChartDataEntry) -> Bool = { $0.x == xValue }
-        let i = partitioningIndex(where: match)
+        var partitioned = self.entries
+        _ = partitioned.partition(by: match)
+        let i = partitioned.partitioningIndex(where: match)
         guard i < endIndex else { return [] }
-        return self[i...].prefix(while: match)
+        return partitioned[i...].prefix(while: match)
     }
     
     /// - Parameters:
@@ -215,9 +217,9 @@ open class ChartDataSet: ChartBaseDataSet
     {
         var closest = partitioningIndex { $0.x >= xValue }
         guard closest < endIndex else { return -1 }
-
+        
         let closestXValue = self[closest].x
-
+        
         switch rounding {
         case .up:
             // If rounding up, and found x-value is lower than specified x, and we can go upper...
@@ -225,18 +227,25 @@ open class ChartDataSet: ChartBaseDataSet
             {
                 formIndex(after: &closest)
             }
-
+            
         case .down:
             // If rounding down, and found x-value is upper than specified x, and we can go lower...
             if closestXValue > xValue && closest > startIndex
             {
                 formIndex(before: &closest)
             }
-
+            
         case .closest:
-            break
+            // The closest value in the beginning of this function
+            // `var closest = partitioningIndex { $0.x >= xValue }`
+            // doesn't guarantee closest rounding method
+            if closest > 0 {
+                let distanceAfter = abs(self[closest].x - xValue)
+                let distanceBefore = abs(self[closest-1].x - xValue)
+                distanceBefore < distanceAfter ? closest -= 1 : ()
+            }
         }
-
+        
         // Search by closest to y-value
         if !yValue.isNaN
         {
@@ -244,15 +253,15 @@ open class ChartDataSet: ChartBaseDataSet
             {
                 formIndex(before: &closest)
             }
-
+            
             var closestYValue = self[closest].y
             var closestYIndex = closest
-
+            
             while closest < index(before: endIndex)
             {
                 formIndex(after: &closest)
                 let value = self[closest]
-
+                
                 if value.x != closestXValue { break }
                 if abs(value.y - yValue) <= abs(closestYValue - yValue)
                 {
@@ -260,7 +269,7 @@ open class ChartDataSet: ChartBaseDataSet
                     closestYIndex = closest
                 }
             }
-
+            
             closest = closestYIndex
         }
         
@@ -306,7 +315,7 @@ open class ChartDataSet: ChartBaseDataSet
         {
             let startIndex = entryIndex(x: e.x, closestToY: e.y, rounding: .up)
             let closestIndex = self[startIndex...].lastIndex { $0.x < e.x }
-                ?? startIndex
+            ?? startIndex
             calcMinMax(entry: e)
             entries.insert(e, at: closestIndex)
         }
@@ -323,7 +332,7 @@ open class ChartDataSet: ChartBaseDataSet
     {
         remove(entry)
     }
-
+    
     /// Removes an Entry from the DataSet dynamically.
     /// This will also recalculate the current minimum and maximum values of the DataSet and the value-sum.
     ///
@@ -336,7 +345,7 @@ open class ChartDataSet: ChartBaseDataSet
         _ = remove(at: index)
         return true
     }
-
+    
     /// Removes the first Entry (at index 0) of this DataSet from the entries array.
     ///
     /// - Returns: `true` if successful, `false` if not.
@@ -358,7 +367,7 @@ open class ChartDataSet: ChartBaseDataSet
         let entry: ChartDataEntry? = isEmpty ? nil : removeLast()
         return entry != nil
     }
-
+    
     /// Removes all values from this DataSet and recalculates min and max value.
     @available(*, deprecated, message: "Use `removeAll(keepingCapacity:)` instead.")
     open override func clear()
@@ -379,7 +388,7 @@ open class ChartDataSet: ChartBaseDataSet
         copy._yMin = _yMin
         copy._xMax = _xMax
         copy._xMin = _xMin
-
+        
         return copy
     }
 }
@@ -388,19 +397,19 @@ open class ChartDataSet: ChartBaseDataSet
 extension ChartDataSet: MutableCollection {
     public typealias Index = Int
     public typealias Element = ChartDataEntry
-
+    
     public var startIndex: Index {
         return entries.startIndex
     }
-
+    
     public var endIndex: Index {
         return entries.endIndex
     }
-
+    
     public func index(after: Index) -> Index {
         return entries.index(after: after)
     }
-
+    
     @objc
     public subscript(position: Index) -> Element {
         get {
@@ -428,40 +437,40 @@ extension ChartDataSet: RangeReplaceableCollection {
         calcMinMax(entry: newElement)
         entries.append(newElement)
     }
-
+    
     public func remove(at position: Index) -> Element {
         let element = entries.remove(at: position)
         notifyDataSetChanged()
         return element
     }
-
+    
     public func removeFirst() -> Element {
         let element = entries.removeFirst()
         notifyDataSetChanged()
         return element
     }
-
+    
     public func removeFirst(_ n: Int) {
         entries.removeFirst(n)
         notifyDataSetChanged()
     }
-
+    
     public func removeLast() -> Element {
         let element = entries.removeLast()
         notifyDataSetChanged()
         return element
     }
-
+    
     public func removeLast(_ n: Int) {
         entries.removeLast(n)
         notifyDataSetChanged()
     }
-
+    
     public func removeSubrange<R>(_ bounds: R) where R : RangeExpression, Index == R.Bound {
         entries.removeSubrange(bounds)
         notifyDataSetChanged()
     }
-
+    
     @objc
     public func removeAll(keepingCapacity keepCapacity: Bool) {
         entries.removeAll(keepingCapacity: keepCapacity)
